d3c82b4aec736634d6d00ec75eb1ab72a0b9eb33
anvarov <akmalanvaroff@gmail.com>
2018-06-21 17:43:49 -0500

    added function and prototypes in app.js converted to class

--- js/app.js
+++ js/app.js
@@ -1,35 +1,109 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Enemy {
+    constructor(x, y){
+        this.sprite = 'images/enemy-bug.png'
+        this.x = x
+        this.y = y
+    }
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+    update(dt) {
+        if (this.x >= 505) {
+            this.x = 0
+        }
+        this.x += 40 * dt
+    }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+class Player {
+    constructor(x, y){
+        this.sprite = 'images/char-boy.png'
+        this.x = x
+        this.y = y
+    }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+    update(){
+        //checking all enemy entities and player coordinates for a collision
+        allEnemies.forEach(enemy => {
+            if (
+                (
+                (Math.floor(enemy.x) + 77 > this.x && 
+                Math.floor(enemy.x) - 77 < this.x)
+                ) && 
+                (
+                Math.floor(enemy.y) - 60 < this.y &&
+                Math.floor(enemy.y) + 60 > this.y)
+            ){
+                ctx.clearRect(0, 0, 505, 606)
+                this.x = 200
+                this.y = 400
+                allEnemies = [new Enemy(-250, 40), new Enemy(5, 130), new Enemy(-135, 220)]
+            }
+        });
+    }
 
+    handleInput(key){
+        //if player reaches blue plate, show alert with a message
+        if (this.y < 90) {
+            // and apply blur filter
+            ctx.filter = 'blur(10px)'
+            setTimeout(() => {
+                //after blur filter applied show the message
+                alert('Congratulations you won the game')
+                //then clear the filter and create a new game
+                ctx.filter = 'none'
+                this.x = 200
+                this.y = 400
+                allEnemies = [new Enemy(-250, 40), new Enemy(5, 130), 
+                              new Enemy(-135, 220)]
+            }, 100);
+        }
+        //switch for key, also checks for boundaries
+        switch (key) {
+            case 'left':
+                if (this.x - 100 < 0) {
+                    return
+                } else {
+                    this.x -= 100
+                }
+                break;
+            case 'right':
+                if (this.x + 100 > 405) {
+                    return
+                } else {
+                    this.x += 100
+                }
+                break;
+            case 'up':
+                if (this.y - 90 < -100) {
+                    return
+                } else {
+                    this.y -= 90
+                }
+                break;
+            case 'down':
+                if (this.y + 90 > 405) {
+                    return
+                } else {
+                    this.y += 90
+                }
+            default:
+                break;
+        }
+    }
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+let allEnemies = [new Enemy(-250, 50), new Enemy(5, 140), new Enemy(-135, 220)]
+const player = new Player(200,400)
 
 
 // This listens for key presses and sends the keys to your
